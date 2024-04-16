ea17b47a862888093a391219b3a5af86beed0b5a
Ankush Bhowmik <ankushbhowmiktesting@gmail.com>
2024-04-17 01:43:20 +0530

    configure database connection, add handlers for unhandledRejection and uncaughtException in server.js and add morgan logging to app.js

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const morgan = require("morgan");
 const codeController = require("./controllers/codeController");
 
 const app = express();
+
+// MIDDLEWARES:
+
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded());
 
--- server.js
+++ server.js
@@ -1,10 +1,28 @@
-require("dotenv").config({ path: "./.env" });
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+
+process.on("uncaughtException", function (err) {
+  console.log("Uncaught Exception ⚠️", err);
+  process.exit(1);
+});
+
+dotenv.config({ path: "./.env" });
+const DB = process.env.DB_URI.replace("<PASSWORD>", process.env.DB_PASSWORD);
+
+mongoose.connect(DB).then((conn) => {
+  console.log("Database connection successful!");
+  console.log(conn.connections);
+});
 
 const app = require("./app");
 
-const port = 8000;
-app.listen(port, "127.0.0.1", () => {
+const port = process.env.PORT;
+const server = app.listen(port, "127.0.0.1", () => {
   console.log(`Server's up, listening to requests at port no: ${port}`);
   console.log("--------------X--------------X--------------X--------------");
+});
+
+process.on("unhandledRejection", function (err) {
+  console.log("Unhandled Rejection ⚠️:", err);
+  server.close(() => process.exit(1));
 });
