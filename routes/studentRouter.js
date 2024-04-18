6c5937c51f317d7b1c5271601282a0cc455024b9
Ankush Bhowmik <ankushbhowmiktesting@gmail.com>
2024-04-19 03:38:04 +0530

    rename files inside routes, .js extension was missing

--- routes/studentRouter.js
+++ routes/studentRouter.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const authController = require("../controllers/authController");
+const studentController = require("../controllers/studentController");
 const router = express.Router({ mergeParams: true });
 
 router.route("/login").get(authController.getStudentLogin).post(authController.postStudentLogin);
-
+router.get("/home", authController.protect, studentController.getStudentCourses);
 module.exports = router;
