ef99068b1f60fd179893d58940d094f4c71e5cd6
Ankush Bhowmik <ankushbhowmiktesting@gmail.com>
2024-04-19 14:56:43 +0530

    login form now complete with alerts for both students and teachers

--- public/src/scripts/login.js
+++ public/src/scripts/login.js
@@ -8,6 +8,11 @@ passView.addEventListener("click", (e) => {
 });
 
 const form = document.querySelector(".login-form");
+const successAlert = document.querySelector(".alert-success");
+const errorAlert = document.querySelector(".alert-error");
+console.log(errorAlert);
+console.log(successAlert);
+
 const urlUser = window.location.pathname.includes("students") ? "students" : "teachers";
 console.log(urlUser);
 
@@ -29,9 +34,23 @@ form.addEventListener("submit", async (e) => {
     console.log(response);
     const results = await response.json();
     console.log(results);
-    // console.log(results.redirectURL);
-    results.redirectURL && location.assign(results.redirectURL);
+    if (results.status === "fail" || results.status === "error") {
+      const errMsg = [...errorAlert.children].at(-1);
+      errMsg.textContent = results.message;
+      errorAlert.classList.remove("hidden");
+      errorAlert.classList.add("flex");
+    } else {
+      successAlert.classList.remove("hidden");
+      successAlert.classList.add("flex");
+      errorAlert.classList.add("hidden");
+      setTimeout(() => {
+        // console.log(results.redirectURL);
+        results.redirectURL && location.assign(results.redirectURL);
+      }, 1000);
+    }
   } catch (err) {
     console.log(err);
+    // loginMsg.textContent = err.message;
+    // console.log(loginMsg);
   }
 });
